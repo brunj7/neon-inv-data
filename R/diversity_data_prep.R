68c3f4054bf8a92da9076e66a3fe4ba1dcf3649d
‘admahood’ <admahood@gmail.com>
2019-10-30 19:40:01 -0600

    wrote out data frame, finished data ingest script for now

--- R/diversity_data_prep.R
+++ R/diversity_data_prep.R
@@ -3,11 +3,16 @@ library(tidyverse)
 library(ggpubr)
 library(vegan)
 
-
-
-loadByProduct(dpID = "DP1.10058.001", site = c("SRER", "ONAQ", "MOAB", "JORN"), 
+options(stringsAsFactors = FALSE)
+loadByProduct(dpID = "DP1.10058.001", 
+              site = c("SRER", "ONAQ", "MOAB", "JORN"), 
               check.size = F) -> x
 
+# data comes in two separate components - 1m2 subplots with cover estimates
+# and 10 and 100m2 subplots with presence only
+
+# Initial ingest ---------------------------------------------------------------
+# 1m2 cover is aggregated by sampling bout and plot
 cover <- x$div_1m2Data %>%
   mutate(endDate = as.Date(endDate)) %>%
   dplyr::filter(divDataType == "plantSpecies") %>%
@@ -18,6 +23,7 @@ cover <- x$div_1m2Data %>%
             endDate = first(endDate)) %>%
   ungroup() 
 
+# 10m2,100m2 are given 0.5 (we can change later)
 traces <- x$div_10m2Data100m2Data %>%
   mutate(endDate = as.Date(endDate)) %>%
   dplyr::filter(targetTaxaPresent == "Y") %>%
@@ -28,18 +34,46 @@ traces <- x$div_10m2Data100m2Data %>%
             nativeStatusCode = first(nativeStatusCode)) %>%
   ungroup() 
 
+# calculating various indexes at plot level at each timestep -------------------
+# native vs invasive cover and relative cover
 n_i <- rbind(cover, traces)%>%
   group_by(plotID, endDate) %>%
   mutate(total_cover = sum(cover))%>%
   ungroup() %>%
-  group_by(plotID, endDate, nativeStatusCode) %>%
+  group_by(plotID, bout_year, nativeStatusCode) %>%
   summarise(cover = sum(cover),
-            total_cover = first(total_cover)) %>%
+            total_cover = first(total_cover),
+            endDate = first(endDate)) %>%
   ungroup() %>%
   mutate(rel_cover = cover/total_cover) %>%
   #filter(nativeStatusCode == "N" | nativeStatusCode == "I")%>%
   ungroup()
+  
+n_i_cover <- n_i %>%
+  filter(nativeStatusCode != "" &
+           nativeStatusCode != "A" &
+           nativeStatusCode != "NI") %>%
+  dplyr::select(plotID, bout_year, endDate, nativeStatusCode, cover) %>%
+  pivot_wider(names_from = nativeStatusCode,
+              values_from = cover,
+              values_fill = list(cover = 0)) %>%
+  rename(cover_native = N,
+         cover_exotic = I,
+         cover_unk = UNK)
 
+n_i_rel_cover <- n_i %>%
+  filter(nativeStatusCode != ""&
+           nativeStatusCode != "A" &
+           nativeStatusCode != "NI") %>%
+  dplyr::select(plotID, bout_year, endDate, nativeStatusCode, rel_cover) %>%
+  pivot_wider(names_from = nativeStatusCode,
+              values_from = rel_cover,
+              values_fill = list(rel_cover = 0))%>%
+  rename(rel_cover_native = N,
+         rel_cover_exotic = I,
+         rel_cover_unk = UNK)
+
+#diversity indexes splitting between native status
 vegan_friendly_div <- rbind(cover, traces) %>%
   group_by(plotID, taxonID, bout_year, endDate, nativeStatusCode) %>%
   summarise(cover = sum(cover)) %>%
@@ -76,5 +110,31 @@ shannon <- vegan_friendly_div %>%
               values_fill = list(shannon=0)) %>%
   rename(shannon_native = N, shannon_exotic=I, shannon_unk = UNK)
 
-#still need total diverstiy
-plot_level <- left_join(nspp, shannon, by = c("plotID", "bout_year", "endDate"))
+# total diversity - not splitting between native status
+vegan_friendly_div_total <- rbind(cover, traces) %>%
+  group_by(plotID, taxonID, bout_year, endDate) %>%
+  summarise(cover = sum(cover)) %>%
+  ungroup() %>%
+  mutate(taxonID = as.character(taxonID),
+         plotID = as.character(plotID)) %>%
+  filter(nchar(as.character(taxonID))>0) %>%
+  # dplyr::select(-endDate) %>%
+  group_by(plotID, bout_year) %>%
+  #filter(bout_year == "2017_1") %>%
+  #dplyr::select(-endDate, -bout_year,-nativeStatusCode) %>%
+  #mutate(year = str_sub(bout_year,1,4)) %>%
+  spread(taxonID, cover, fill=0) %>%
+  ungroup()
+
+div_total = dplyr::select(vegan_friendly_div_total, plotID, bout_year, endDate)
+div_total$shannon_total = diversity(vegan_friendly_div_total[4:ncol(vegan_friendly_div_total)])
+div_total$nspp_total = specnumber(vegan_friendly_div_total[4:ncol(vegan_friendly_div_total)])
+
+
+
+# joining and writing out ------------------------------------------------------
+plot_level <- left_join(nspp, shannon, by = c("plotID", "bout_year", "endDate")) %>%
+  left_join(n_i_cover, by = c("plotID", "bout_year", "endDate")) %>%
+  left_join(n_i_rel_cover, by = c("plotID", "bout_year", "endDate")) %>%
+  left_join(div_total, by = c("plotID", "bout_year", "endDate"))
+write_csv(plot_level, "data/plot_level_diversity_stuff.csv")
